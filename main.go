a278e5e92b074462e93a42eccea4d4f1f36511b2
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 16:16:59 +0200

    Hier ist alles super

--- main.go
+++ main.go
@@ -2,37 +2,86 @@ package main
 
 import (
 	"image/color"
+	"math"
 
 	"github.com/hajimehoshi/ebiten"
 	"github.com/hajimehoshi/ebiten/ebitenutil"
-	"github.com/mortim-portim/WorldEditor/math"
+	"github.com/mortim-portim/WorldEditor/mathwe"
 )
 
-type Window struct {
-	matrix *math.Matrix
+type window struct {
+	zoom   float64
+	matrix *mathwe.Matrix
 	images []ebiten.Image
 }
 
-func (w *Window) Update(screen *ebiten.Image) error {
-	screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
+const screenwidth = 960
+const screenheight = 520
+const width = 10
+const height = 5
+const pixelwidth = 16
+
+func (w *window) Update(screen *ebiten.Image) error {
+	//input
+	_, dy := ebiten.Wheel()
+	w.zoom += dy / 5
+
+	if ebiten.IsMouseButtonPressed(ebiten.MouseButtonLeft) {
+		ix, iy := ebiten.CursorPosition()
+		x := math.Floor(float64(ix) / w.zoom / pixelwidth)
+		y := math.Floor(float64(iy) / w.zoom / pixelwidth)
+
+		w.matrix.Set(int(x), int(y), 1, 0)
+	}
+
+	//drawing
+	screen.Fill(color.NRGBA{0x00, 0xA0, 0x31, 0xff})
+
+	for x := 0; x < width; x++ {
+		if float64(x*16)*w.zoom > float64(screenwidth)/3.0*2.0 {
+			continue
+		}
+
+		for y := 0; y < height; y++ {
+			if float64(y*16)*w.zoom > float64(screenheight)/3.0*2.0 {
+				continue
+			}
+
+			img := w.images[w.matrix.Get(x, y, 0)]
+			op := ebiten.DrawImageOptions{}
+
+			op.GeoM.Scale(w.zoom, w.zoom)
+			op.GeoM.Translate(float64(x*16)*w.zoom, float64(y*16)*w.zoom)
+
+			screen.DrawImage(&img, &op)
+		}
+	}
 	return nil
 }
 
-func (w *Window) Layout(outsideWidth, outsideHeight int) (int, int) {
-	return 640, 480
+func (w *window) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return outsideWidth, outsideHeight
 }
 
 //Main funciton
 func main() {
-	window := &Window{}
+	path := []string{
+		"./resource/16.png",
+	}
 
-	path := []string{"./resource/16.png"}
+	images := []ebiten.Image{}
 
 	for p := range path {
 		image, _, _ := ebitenutil.NewImageFromFile(path[p], ebiten.FilterDefault)
-		window.images = append(window.images, *image)
+		images = append(images, *image)
 	}
 
+	matrix := mathwe.Matrix{X: width, Y: height, Z: 1}
+	matrix.Init(0)
+
+	window := &window{zoom: 4, matrix: &matrix, images: images}
+
+	ebiten.SetWindowSize(screenwidth, screenheight)
 	if err := ebiten.RunGame(window); err != nil {
 		panic(err)
 	}
---
+++ mathwe/matrix.go
@@ -0,0 +1,54 @@
+package mathwe
+
+import (
+	"fmt"
+)
+
+type Matrix struct {
+	X, Y, Z int
+	list    []int
+}
+
+func (m *Matrix) Init(standard int) {
+	m.list = make([]int, m.X*m.Y*m.Z)
+	for i := range m.list {
+		m.list[i] = standard
+	}
+}
+
+func (m *Matrix) InitIdx() {
+	m.list = make([]int, m.X*m.Y*m.Z)
+	for i := range m.list {
+		m.list[i] = i
+	}
+}
+
+func (m *Matrix) Get(x, y, z int) int {
+	return m.list[x+m.X*y+m.X*m.Y*z]
+}
+func (m *Matrix) Set(x, y, z, v int) {
+	m.list[x+m.X*y+m.X*m.Y*z] = v
+}
+
+func (m *Matrix) SubMatrix(x1, y1, z1, x2, y2, z2 int) *Matrix {
+	newMat := &Matrix{x2 - x1 + 1, y2 - y1 + 1, z2 - z1 + 1, nil}
+	newMat.Init(0)
+
+	for x := x1; x <= x2; x++ {
+		for y := y1; y <= y2; y++ {
+			for z := z1; z <= z2; z++ {
+				newMat.Set(x-x1, y-y1, z-z1, m.Get(x, y, z))
+			}
+		}
+	}
+
+	return newMat
+}
+
+func (m *Matrix) Print() string {
+	out := ""
+	for _, i := range m.list {
+		out += fmt.Sprintf("%v, ", i)
+	}
+	return out
+}
