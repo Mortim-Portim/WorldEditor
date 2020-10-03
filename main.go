929532021135abde795b6413d60f02fc7a17adcd
BuildTools <unconfigured@null.spigotmc.org>
2020-10-03 10:34:31 +0200

    Test

--- main.go
+++ main.go
@@ -1,10 +1,33 @@
-package main 
+package main
 
 import (
-	"fmt"
+	"image/color"
+
+	"github.com/hajimehoshi/ebiten"
+	"github.com/hajimehoshi/ebiten/ebitenutil"
 )
 
 func main() {
-	fmt.Println("Guido ist Fett!!")
+	if err := ebiten.Run(update, 320, 240, 2, "Hello World"); err != nil {
+		panic(err)
+	}
 }
 
+var square *ebiten.Image
+
+func update(screen *ebiten.Image) error {
+	screen.Fill(color.NRGBA{0xff, 0x00, 0x00, 0xff})
+	ebitenutil.DebugPrint(screen, "Hello World")
+
+	if square == nil {
+		square, _ = ebiten.NewImage(16, 16, ebiten.FilterNearest)
+	}
+
+	square.Fill(color.White)
+
+	opts := &ebiten.DrawImageOptions{}
+	opts.GeoM.Translate(64, 64)
+	screen.DrawImage(square, opts)
+
+	return nil
+}
